2bc509fdfda337ee956ee0a4c917466101d43de3
Dmitrii Turetskikh <tureckihdmitriq@gmail.com>
2024-02-22 12:45:59 +0300

    fix get main func

--- main.py
+++ main.py
@@ -8,90 +8,6 @@ from core.models import Order, Product, OrderProductAssociation, ProductRackAsso
 from core.models.db_helper import db_helper
 
 
-async def create_order(session: AsyncSession, title: str) -> Order:
-    order = Order(title=title)
-
-    session.add(order)
-    await session.commit()
-
-    return order
-
-
-async def create_product(session: AsyncSession, title: str) -> Product:
-    product = Product(title=title)
-
-    session.add(product)
-    await session.commit()
-
-    return product
-
-
-# async def create_orders_and_products(session: AsyncSession):
-#     order_1 = await create_order(session, title="order_1")
-#     order_2 = await create_order(session, title="order_2")
-#
-#     notebook = await create_product(session, title="Ноутбук")
-#     telephone = await create_product(session, title="Телефон")
-#     watch = await create_product(session, title="Часы")
-#
-#     order_1 = await session.scalar(
-#         select(Order)
-#         .where(Order.id == order_1.id)
-#         .options(selectinload(Order.products))
-#     )
-#     order_2 = await session.scalar(
-#         select(Order)
-#         .where(Order.id == order_2.id)
-#         .options(selectinload(Order.products))
-#     )
-#
-#     order_1.products.append(notebook)
-#     order_1.products.append(telephone)
-#     order_2.products.append(notebook)
-#     order_2.products.append(telephone)
-#     order_2.products.append(watch)
-#
-#     await session.commit()
-#
-#
-# async def get_orders_with_products(session: AsyncSession) -> list[Order]:
-#     stmt = (
-#         select(Order)
-#         .options(
-#             selectinload(Order.products),
-#         )
-#         .order_by(Order.id)
-#     )
-#     orders = await session.scalars(stmt)
-#
-#     return list(orders)
-#
-#
-# async def demo_get_orders_with_products_through_secondary(session: AsyncSession):
-#     orders = await get_orders_with_products(session)
-#     for order in orders:
-#         print(order.id, order.title, "products:")
-#         for product in order.products:  # type: Product
-#             print("-", product.id, product.title)
-#
-#
-# async def get_orders_with_products_assoc(session: AsyncSession) -> list[Order]:
-#     stmt = (
-#         select(Order)
-#         .options(
-#             selectinload(Order.products_details)
-#             .joinedload(OrderProductAssociation.product)  # new
-#             .joinedload(Product.racks_products_details)  # new
-#             .joinedload(ProductRackAssociation.rack),  # new
-#         )
-#         .order_by(Order.id)
-#     )
-#     result = await session.execute(stmt)
-#     orders = result.scalars().fetchall()
-#
-#     return list(orders)
-
-
 async def get_orders_with_products_assoc(
     session: AsyncSession, order_ids: list[int]
 ) -> list[Order]:
@@ -108,42 +24,49 @@ async def get_orders_with_products_assoc(
     )
     result = await session.execute(stmt)
     orders = result.scalars().fetchall()
-    return orders
+    return list(orders)
 
 
 async def demo_get_orders_with_products_with_assoc(session: AsyncSession):
     order_ids = [10, 11, 14, 15]
     orders = await get_orders_with_products_assoc(session, order_ids)
 
-    racks_dict = {}
     print("=+=+=+=")
     print(f"Страница сборки заказов {', '.join(map(str, order_ids))}\n")
+
+    racks_products = {}
+
     for order in orders:
-
         for order_product_detail in order.products_details:
-            for rack_assoc in order_product_detail.product.racks_products_details:
-                rack_title = rack_assoc.rack.title
-                if rack_title not in racks_dict:
-                    racks_dict[rack_title] = []
+            product = order_product_detail.product
+            rack_title = None
+            for rack_assoc in product.racks_products_details:
+                if rack_assoc.is_primary:
+                    rack_title = rack_assoc.rack.title
+                    break
+            if rack_title:
+                if rack_title not in racks_products:
+                    racks_products[rack_title] = []
+                racks_products[rack_title].append(order_product_detail)
 
-                racks_dict[rack_title].append(order_product_detail)
-
-    for rack_title, order_product_details in racks_dict.items():
+    for rack_title, products in racks_products.items():
         print(f"===Стеллаж {rack_title}")
-        for order_product_detail in order_product_details:
+        for product_detail in products:
+            product = product_detail.product
             print(
-                f"{order_product_detail.product.title} (id={order_product_detail.product.id})"
+                f"{product.title} (id={product.id})\n"
+                f"заказ {product_detail.order.id}, {product_detail.count} шт"
             )
-            print(
-                f"заказ {order_product_detail.order.id}, {order_product_detail.count} шт"
-            )
+            additional_racks = []
+            for rack_assoc in product.racks_products_details:
+                if not rack_assoc.is_primary:
+                    additional_racks.append(rack_assoc.rack.title)
+            if additional_racks:
+                print(f"доп стеллаж: {', '.join(additional_racks)}")
             print()
-        print()
 
 
 async def demo_m2m(session: AsyncSession):
-    # await create_orders_and_products(session)
-    # await demo_get_orders_with_products_through_secondary(session)
     await demo_get_orders_with_products_with_assoc(session)
 
 
@@ -154,6 +77,3 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
-
-
-# docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' db_test_case_go
--- queries.sql
+++ queries.sql
@@ -1,4 +1,3 @@
---
 SELECT
   order_product_association.order_id AS order_product_association_order_id,
   order_product_association.id AS order_product_association_id,
@@ -7,6 +6,7 @@ SELECT
   racks_1.id AS racks_1_id,
   racks_1.title AS racks_1_title,
   product_rack_association_1.id AS product_rack_association_1_id,
+  product_rack_association_1.is_primary AS product_rack_association_1_is_primary,
   product_rack_association_1.product_id AS product_rack_association_1_product_id,
   product_rack_association_1.rack_id AS product_rack_association_1_rack_id,
   products_1.id AS products_1_id,
@@ -17,4 +17,4 @@ FROM
   LEFT OUTER JOIN product_rack_association AS product_rack_association_1 ON products_1.id = product_rack_association_1.product_id
   LEFT OUTER JOIN racks AS racks_1 ON racks_1.id = product_rack_association_1.rack_id
 WHERE
-  order_product_association.order_id IN (1, 2)
+  order_product_association.order_id IN (10,11,14,15)
