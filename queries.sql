51f96e1da2d4bd4eb834cfe1623edc5b31a9202a
Dmitrii Turetskikh <tureckihdmitriq@gmail.com>
2024-02-23 12:07:52 +0300

    add new request to db

--- queries.sql
+++ queries.sql
@@ -18,3 +18,37 @@ FROM
   LEFT OUTER JOIN racks AS racks_1 ON racks_1.id = product_rack_association_1.rack_id
 WHERE
   order_product_association.order_id IN (10,11,14,15)
+
+--
+SELECT
+  order_product_association.order_id AS с,
+  order_product_association.count AS order_product_association_count,
+  racks_1.title AS racks_1_title,
+  product_rack_association_1.is_primary AS product_rack_association_1_is_primary,
+  products_1.id AS products_1_id,
+  products_1.title AS products_1_title
+FROM
+  order_product_association
+  LEFT OUTER JOIN products AS products_1 ON products_1.id = order_product_association.product_id
+  LEFT OUTER JOIN product_rack_association AS product_rack_association_1 ON products_1.id = product_rack_association_1.product_id
+  LEFT OUTER JOIN racks AS racks_1 ON racks_1.id = product_rack_association_1.rack_id
+WHERE
+  order_product_association.order_id IN (10,11,14,15)
+
+
+--
+with cte as (
+    select opa.order_id,
+           opa.product_id
+    from order_product_association opa
+    where opa.order_id in (10, 11, 14, 15)
+    )
+select cte.order_id,
+       cte.product_id,
+       p.title as product_title,
+       r.rack_id,
+       r.title as rack_title
+from cte
+inner join products p on cte.product_id = p.id
+inner join product_rack_association pra on p.id = pra.product_id
+inner join racks r on pra.rack_id = r.rack_id;
